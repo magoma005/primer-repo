2d77d025977c66d7c3d58a8940d254a81054213b
Miguel Angel Guarin <mago123005@gmail.com>
2025-08-11 21:35:06 -0500

    ahora resto}

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,4 +2,7 @@ public class Calculadora {
     public static double sumar(double a, double b){
         return a + b;
     }
+    public static double restar(double a, double b){
+        return a - b;
+    }
 }
