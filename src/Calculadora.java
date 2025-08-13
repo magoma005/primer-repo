eec33645feb316b2a2dd163587e4cea57c7fb4eb
Miguel Angel Guarin <mago123005@gmail.com>
2025-08-13 18:31:59 -0500

    multiplica y divide

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,19 @@
 public class Calculadora {
-    public static double sumar(double a, double b){
+    public static double sumar(double a, double b) {
         return a + b;
     }
-    public static double restar(double a, double b){
+    public static double restar(double a, double b) {
         return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir entre cero");
+        }
+        return a / b;
     }
 }
